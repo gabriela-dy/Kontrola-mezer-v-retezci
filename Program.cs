11649af2360cea02b902894a0ac1d65e8e397eaa
Gabriela Dyworová <gabriela.dyworova@gmail.com>
2022-11-08 13:04:48 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Zadejte číslo:");
+string s = Console.ReadLine();
+Console.WriteLine("Zadal jste text: " + s);
+Console.WriteLine("Text po funkci trim: " + s.Trim());
+int a = int.Parse(s);
+Console.WriteLine("Převedl jsem zadaný text na číslo parsováním, zadal jste: " + a);
+Console.ReadKey();
